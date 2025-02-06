ff939b2bdd473066edeccfec023f9f7566d275f1
lioliadoc <liolia.doc@gmail.com>
2025-02-05 20:55:44 -0600

    Modified heroku config

--- app/__init__.py
+++ app/__init__.py
@@ -23,7 +23,11 @@ def create_app(config=None):
     CORS(app, supports_credentials=True, origins=["http://localhost:5173"])
 
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+    database_url = os.environ.get('DATABASE_URL')
+    if database_url.startswith('postgres://'):
+        database_url = database_url.replace('postgres://', 'postgresql://', 1)
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = database_url
 
     app.secret_key = "your_generated_random_string"
     if config:
