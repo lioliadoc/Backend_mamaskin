a6a4ac838d4428fd16715e342dd23facaab41558
lioliadoc <liolia.doc@gmail.com>
2025-02-10 16:30:51 -0600

    google_auth modified

--- app/routes/google_auth_route.py
+++ app/routes/google_auth_route.py
@@ -38,23 +38,6 @@ def google_login_callback():
     print("You have successfully logged in via Google!", "success")
     return redirect("https://mamaskin-frontend-afbb848647f2.herokuapp.com/stories")
 
-@bp.route("/protected")
-def protected():
-    if not google.authorized:
-        print("Please log in with Google first. No valid token found.")
-        return redirect(url_for("google.login"))
-
-    try:
-        resp = google.get("/oauth2/v2/userinfo")
-        if not resp.ok:
-            flash("Could not fetch user info. Please re-authenticate.")
-            return redirect(url_for("google.login"))
-
-        user_info = resp.json()
-        return f"This is a protected route. Hello, {user_info['name']}!"
-    except TokenExpiredError:
-        print("Your token has expired. Please log in again.")
-        return redirect(url_for("google.login"))
 
 
 
