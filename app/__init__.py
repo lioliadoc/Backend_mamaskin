f1745c90ab34fd679370621c4cbc7b06d076f9e8
lioliadoc <liolia.doc@gmail.com>
2025-02-10 18:46:54 -0600

    google_auth modified

--- app/__init__.py
+++ app/__init__.py
@@ -66,8 +66,8 @@ def create_app(config=None):
     
     oauth = OAuth(app) 
     google = oauth.register( name="google", 
-                            client_id="YOUR_CLIENT_ID", 
-                            client_secret="YOUR_CLIENT_SECRET", 
+                            client_id=os.environ["GOOGLE_OAUTH_CLIENT_ID"], 
+                            client_secret=os.environ["GOOGLE_OAUTH_CLIENT_SECRET"], 
                             access_token_url="https://oauth2.googleapis.com/token", 
                             access_token_params=None, authorize_url="https://accounts.google.com/o/oauth2/auth", 
                             authorize_params=None, api_base_url="https://www.googleapis.com/oauth2/v2/", 
